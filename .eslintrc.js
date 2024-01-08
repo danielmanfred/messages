4d707adbbe024c3b7fbb37540c3c208cf8edcd62
Daniel Brito <danielmanfred@gmail.com>
2024-01-08 17:19:52 -0300

    use the nest cli to generate files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  /*
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
@@ -21,5 +22,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+  },*/
 };
