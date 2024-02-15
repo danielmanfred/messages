6131bf992f77dcd2f11c14d18965a5799c865f53
danielmanfred <danielmanfred@gmail.com>
2024-02-15 15:22:37 -0300

    add validation rules

--- dist/messages/messages.controller.d.ts
+++ dist/messages/messages.controller.d.ts
@@ -1,5 +1,6 @@
+import { CreateMessageDto } from './dtos/create-message.dto';
 export declare class MessagesController {
     listMessages(): void;
-    createMessage(body: any): void;
+    createMessage(body: CreateMessageDto): void;
     getMessage(id: string): void;
 }
